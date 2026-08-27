2ac1fc2491e2d180e4429abc7ed8cfc5af1ef403
asarwan720-2 <asarwan720@gmail.com>
2026-08-27 15:11:52 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+/* ================= SUPABASE.JS ================= */
+
+/* Supabase library se client banana */
+const supabaseClient = supabase.createClient(
+
+  "https://katbdbzoufiblnnsoxkk.supabase.co",
+
+  "sb_publishable_--fdpQMmSlUyt2ywjUErgQ_LXGvQH-a"
+
+);
